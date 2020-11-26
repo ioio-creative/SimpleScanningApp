0a193200e680d2616df3db0595586f1378b62110
Sze Wa, Wong <szewa.wong@connect.polyu.hk>
2020-11-26 23:22:24 +0800

    first commit\

---
+++ SimpleScanningApp/Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SimpleScanningApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lstListOfScanners = new System.Windows.Forms.ListBox();
+            this.btnScan = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnScan);
+            this.panel1.Controls.Add(this.lstListOfScanners);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(383, 426);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(134, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "List Of Scanners";
+            // 
+            // lstListOfScanners
+            // 
+            this.lstListOfScanners.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstListOfScanners.FormattingEnabled = true;
+            this.lstListOfScanners.ItemHeight = 18;
+            this.lstListOfScanners.Location = new System.Drawing.Point(6, 43);
+            this.lstListOfScanners.Name = "lstListOfScanners";
+            this.lstListOfScanners.Size = new System.Drawing.Size(345, 166);
+            this.lstListOfScanners.TabIndex = 1;
+            // 
+            // btnScan
+            // 
+            this.btnScan.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnScan.Location = new System.Drawing.Point(6, 239);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(99, 48);
+            this.btnScan.TabIndex = 2;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(420, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(368, 426);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ListBox lstListOfScanners;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ SimpleScanningApp/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using WIA;
+
+namespace SimpleScanningApp
+{
+    public partial class Form1 : Form
+    {
+        private const string ScannerName = "EPSON Perfection V37/V370";
+        private const string OutputPath = @"E:/ScanImg.png";
+        private const int ScannerDpiHorizontal = 300; //1200;
+        private const int ScannerDpiVertical = 300; //1200;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // https://youtu.be/PwnCjZSnE_E
+            try
+            {
+                foreach (DeviceInfo deviceInfo in GetScannerDeviceInfos())
+                {
+                    lstListOfScanners.Items.Add(GetNameFromDeviceInfo(deviceInfo));
+                }
+            }
+            catch (COMException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            // https://youtu.be/PwnCjZSnE_E
+            // https://www.codesenior.com/sources/docs/tutorials/How-to-get-Images-From-Scanner-in-C-with-Windows-Image-AcqusitionWIA-Library-.pdf
+            try
+            {            
+                DeviceInfo availableScanner = GetScannerDeviceByName(ScannerName);                
+
+                if (availableScanner == null)
+                {
+                    MessageBox.Show("Scanner does not exist");
+                    return;
+                }
+
+                var device = availableScanner.Connect();
+                //device.Properties.get_Item("3088").set_Value(5); // Double scanning or dublex scanning
+                //device.Properties.get_Item("3088").set_Value(1); // Single scanning or simplex scanning
+
+                var scannerItem = device.Items[1];
+                scannerItem.Properties.get_Item("6147").set_Value(ScannerDpiHorizontal); // horizontal dpi
+                scannerItem.Properties.get_Item("6148").set_Value(ScannerDpiVertical); // vertical dpi
+
+                var imgFile = scannerItem.Transfer(FormatID.wiaFormatJPEG) as ImageFile;
+                var path = OutputPath;
+
+                if (File.Exists(path))
+                {
+                    File.Delete(path);
+                }
+
+
+                imgFile.SaveFile(path);
+
+                pictureBox1.ImageLocation = path;
+            }
+            catch (COMException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private static IEnumerable<DeviceInfo> GetScannerDeviceInfos()
+        {
+            return new DeviceManager().DeviceInfos.OfType<DeviceInfo>().Where(x => x.Type == WiaDeviceType.ScannerDeviceType);
+        }
+
+        private static DeviceInfo GetScannerDeviceByName(string name)
+        {
+            string cleanedName = name.ToUpper();
+            return GetScannerDeviceInfos().FirstOrDefault(x => GetNameFromDeviceInfo(x).ToUpper().Contains(cleanedName));
+        }
+
+        private static string GetNameFromDeviceInfo(DeviceInfo deviceInfo)
+        {
+            return deviceInfo.Properties["Name"].get_Value() as string;
+        }
+    }
+}
